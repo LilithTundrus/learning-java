1b8a84f090c086fbe06effc647baa4db2d6f8516
Lilith Tundrus <echofox123@gmail.com>
2019-02-16 19:29:04 -0500

    Added a very basic Hello World example

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+// All Java files are classes
+
+public class HelloWorld {
+
+    // Main function declaration, looks a lot like C++
+    public static void main(String[] args) {
+        // Using the System base class, print a line with the given message
+        System.out.println("Hello World!");
+    }
+}
