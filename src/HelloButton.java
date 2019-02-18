5d7efec449318b23769fba2242b0ef6c567c9b53
Lilith Tundrus <echofox123@gmail.com>
2019-02-18 01:23:39 -0500

    Adding file with a button example

---
+++ src/HelloButton.java
@@ -0,0 +1,83 @@
+
+// Import statements
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class HelloButton {
+    public static void main(String args[]) {
+        // Create a new JFrame to add the custom JComponent
+        JFrame frame = new JFrame("HelloJava2");
+        // Add a new instance of the JComponent to the window fram with the given text
+        frame.add(new HelloComponent3("AAAAAAA"));
+        // Set a default operation when the close button on the window is clicked
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Set the view size of the window frame
+        frame.setSize(300, 300);
+        // Make sure the window actually shows up on screen
+        frame.setVisible(true);
+    }
+}
+
+class HelloComponent3 extends JComponent implements MouseMotionListener, ActionListener {
+
+    // String container to hold the string message
+    String theMessage;
+
+    // Message x/y coordinates
+    int messageX = 125;
+    int messageY = 95;
+
+    // Constructor function (always names the same as the class)
+
+    JButton button;
+
+    int colorIndex;
+    static Color[] colors = { Color.red, Color.green, Color.blue, Color.magenta };
+
+    public HelloComponent3(String message) {
+        theMessage = message;
+        button = new JButton("Change the color");
+        setLayout(new FlowLayout());
+        add(button);
+        button.addActionListener(this);
+        addMouseMotionListener(this);
+    }
+
+    public void paintComponent(Graphics g) {
+        g.drawString(theMessage, messageX, messageY);
+    }
+
+    public void mouseDragged(MouseEvent e) {
+        // Move the x of the message to the mouse's X
+        messageX = e.getX();
+        // Move the y of the message to the mouse's Y
+        messageY = e.getY();
+        // Call the UI repaint method to repaint the window (on its own thred)
+        repaint();
+    }
+
+    public void mouseMoved(MouseEvent e) {
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        // Was the button pressed?
+        if (e.getSource() == button) {
+            changeColor();
+        }
+    }
+
+    synchronized private void changeColor() {
+        // Awkwardly change the color to the next index
+        if (++colorIndex == colors.length) {
+            colorIndex = 0;
+        }
+        setForeground(currentColor());
+        repaint();
+    }
+
+    synchronized private Color currentColor() {
+        return colors[colorIndex];
+    }
+}
