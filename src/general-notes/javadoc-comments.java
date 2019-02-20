3223d3e5fc1f5ac7633257b5254b3cd4cb6cec61
Lilith Tundrus <echofox123@gmail.com>
2019-02-20 04:24:04 -0500

    Added file about Javadoc comments

---
+++ src/general-notes/javadoc-comments.java
@@ -0,0 +1,20 @@
+/* 
+
+A block comment starting with two asterisks (/** ) indicates that the comment block contains
+javadoc information
+
+any property about the below function on the comment uses an @ symbol to differntiate it over
+basic comment text
+
+
+These comments can be used like JavaDoc to create documentation and more
+
+
+The java compiler also check for special indications like @depricated to warn any user that
+a function is in fact depricated
+
+
+These javadoc comments are considered metadata about the java code they come with
+
+
+*/
