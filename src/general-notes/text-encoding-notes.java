c0330419a6da692a9a022434bebdcccf6ae57541
Lilith Tundrus <echofox123@gmail.com>
2019-02-20 04:20:05 -0500

    Added small blurb about Java and text encoding

---
+++ src/general-notes/text-encoding-notes.java
@@ -0,0 +1,17 @@
+/* 
+Java was poised to be the language of the internet, but then JS took over.
+
+Due to this, Java understands the Unicode character set. Since Java 6 unicode is
+how Java stores character and string data by default
+
+Although this is the case Java source code can be written in unicode or basic ASCII
+
+The char and String type in Java natively support unicode
+
+A Unicode character can be represented with this escape sequence:
+ \uxxxx
+
+This is to ensure that all IDEs can still open/read the file
+
+Java of course comes with classes to assist with reading unicode encodings such as UTF-8
+*/
