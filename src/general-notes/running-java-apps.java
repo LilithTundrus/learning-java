3ecdc489c222a21e4310216600233e9ec9c725a3
Lilith Tundrus <echofox123@gmail.com>
2019-02-20 02:48:59 -0500

    Added mostly comments to this file for running applications from the CLI

---
+++ src/general-notes/running-java-apps.java
@@ -0,0 +1,24 @@
+// A standalone Java application MUST include at least one class!
+// This class must have a main() method that takes an array of string as an argument
+
+// That function always looks like this:
+
+public static void main(String[] args) {
+
+}
+
+// To run an application you start the VM with the `java` command and specify the class
+// that you would like to run:
+
+// > java [java options] className [arguments]
+
+// The class name give to the Java VM should be its FULL class path name:
+// IE com.Thing.Run vs Thing.Run
+
+// Though there are exceptions since the default class path is the current directory
+
+// Note that you do NOT have to include the file name extension
+
+
+// Additionally you can use the java command to launch JAR files the same way,
+// JAR files being archived Java code/assets
