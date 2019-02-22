634bee53e12e5557575519304e6126292573493c
Lilith Tundrus <echofox123@gmail.com>
2019-02-22 06:08:32 -0500

    Added very basic info on Java access modifiers

---
+++ src/general-notes/access-modifiers.java
@@ -0,0 +1,7 @@
+/* 
+Modifier    Visibility outside the class
+private     None
+No modifier (default) Classes in the package
+protected   Classes in package and subclasses inside or outside the package
+public All  classes
+*/
