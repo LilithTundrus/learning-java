05bf7250d36c99ca2ac41ef6941c5fcdc782d399
Lilith Tundrus <echofox123@gmail.com>
2019-02-24 06:04:47 -0500

    Added summary chart for Java string functions

---
+++ src/StringWork/StringMethodsSummary.java
@@ -0,0 +1,32 @@
+/* Method       Functionality
+charAt()        Gets a particular character in the string
+compareTo()     Compares the string with another string
+Method          Functionality
+concat()        Concatenates the string with another string
+contains()      Checks whether the string contains another string
+copyValueOf()   Returns a string equivalent to the specified character array
+endsWith()      Checks whether the string ends with a specified suffix
+equals()        Compares the string with another string
+equalsIgnoreCase() Compares the string with another string, ignoring case
+getBytes()      Copies characters from the string into a byte array
+getChars()      Copies characters from the string into a character array
+hashCode()      Returns a hashcode for the string
+indexOf()       Searches for the first occurrence of a character or substring in the string
+intern()        Fetches a unique instance of the string from a global shared-string pool
+isEmpty()       Returns true if the string is zero length
+lastIndexOf()   Searches for the last occurrence of a character or substring in a string
+length()        Returns the length of the string
+matches()       Determines if the whole string matches a regular expression pattern
+regionMatches() Checks whether a region of the string matches the specified region of another string
+replace()       Replaces all occurrences of a character in the string with another character
+replaceAll()    Replaces all occurrences of a regular expression pattern with a pattern
+replaceFirst()  Replaces the first occurrence of a regular expression pattern with a pattern
+split()         Splits the string into an array of strings using a regular expression pattern as a delimiter
+startsWith()    Checks whether the string starts with a specified prefix
+substring()     Returns a substring from the string
+toCharArray()   Returns the array of characters from the string
+toLowerCase()   Converts the string to lowercase
+toString()      Returns the string value of an object
+toUpperCase()   Converts the string to uppercase
+trim()          Removes leading and trailing whitespace from the string
+valueOf()       Returns a string representation of a value */
